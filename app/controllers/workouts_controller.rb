120ddc2bd62b326742d91a9ce2b4ebdedda32562
Edward park <parkes.edward@gmail.com>
2015-06-11 18:52:19 -0400

    moved root to workouts#index and deleted home controller

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,7 +1,20 @@
 class WorkoutsController < ActionController::Base
+	before_action :authenticate_user!
+	before_action :create_workout
+
 	def index
 		@user = current_user
 		@daily_workout= @user.workouts.last
 		@exercises = @daily_workout.exercises.all 
+	end
+
+	private
+
+	def create_workout
+			if current_user.workouts.empty?
+				workout_new = Workout.create 
+				workout_new.user_id = current_user.id 
+				workout_new.save 
+			end
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,12 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  
   resources :workouts do
     resources :exercises
   end
 
+  root 'workouts#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
