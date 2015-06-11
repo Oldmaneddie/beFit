1b14703b7ddec5ec29ecbccd5a2650bbdefe840f
Edward park <parkes.edward@gmail.com>
2015-06-11 14:31:49 -0400

    moved workout model creation to private method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,26 +1,18 @@
 class HomeController < ActionController::Base 
 	before_action :authenticate_user!
-	#before_action :create_workout
+	before_action :create_workout
 	def index
 		
 		@user = current_user.name; 
-		
-		if current_user.workouts.empty?
-			@workout = Workout.create 
-			@workout.user_id = current_user.id 
-			@workout.save 
-		end
-		
-		
-
 	end
 
 	private
-
-	# def create_workout
-	# 	if current_user.workouts.empty?
-
-	# end
-
+		def create_workout
+			if current_user.workouts.empty?
+				@workout = Workout.create 
+				@workout.user_id = current_user.id 
+				@workout.save 
+			end
+		end
 
 end
